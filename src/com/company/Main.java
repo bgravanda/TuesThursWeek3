1eaadc4a1c2229889d5e07f2d5b27c6700e51375
bgravanda <bgravanda@student.bridgew.edu>
2021-09-16 11:29:00 -0400

    Bank Account

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var myAccounts = new BankAccount();
+        myAccounts.deposit(1000);
+        var newBalance = myAccounts.addInterest();
+
+        var succeeded = myAccounts.withdraw(2000);
+        if(succeeded)
+            System.out.println("you managed to withdraw successfully");
+        else
+            System.out.println("you tried to withdraw money from your account. Your balance is " + myAccounts.checkBalance());
     }
 }
